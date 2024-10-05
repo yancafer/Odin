03fb3e0b384c6aa0d1229c4ddff60475a7a87c5e
Yanca <yancafernandes2@gmail.com>
2024-10-05 08:45:12 -0500

    Adicionando comentários e o Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
+/*Importação para Rotas */
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+/* Importação de componentes*/
 import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+
+/* Importação de páginas */
 import Dashboard from "./pages/Dashboard";
 import Estatisticas from "./pages/Estatisticas";
 
+/* Importação de estilos */
+import "./index.css";
+
+
 function App() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/estatisticas" element={<Estatisticas />} />
-      </Routes>
-    </Router>
+    <div className="app-container">
+      <Router>
+        <Menu />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/estatisticas" element={<Estatisticas />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
